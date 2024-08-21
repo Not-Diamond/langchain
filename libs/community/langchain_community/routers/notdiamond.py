e62f3d80aa335bf52a19747c7bd29cfa44c7b3e4
Alejandro Companioni <a9@notdiamond.ai>
2024-08-21 18:47:28 -0400

    runnable interface

--- libs/community/langchain_community/routers/notdiamond.py
+++ libs/community/langchain_community/routers/notdiamond.py
@@ -10,16 +10,30 @@ from langchain_core.runnables.base import Runnable, Output
 from notdiamond import NotDiamond, LLMConfig
 
 class NotDiamondRunnable(Runnable[LanguageModelInput, str]):
+    """
+    See Runnable docs for details
+    https://python.langchain.com/v0.1/docs/expression_language/interface/
+    """
     llm_configs: List[LLMConfig | str]
     api_key: str
 
     def __init__(self, llm_configs: List[LLMConfig | str], api_key: str=os.getenv("NOTDIAMOND_API_KEY")):
         self.client = NotDiamond(llm_configs=llm_configs, api_key=api_key)
 
-    def invoke(self, input: List[LanguageModelInput]) -> str:
+    def _model_select(self, input: List[LanguageModelInput]) -> str:
         messages = _convert_input_to_message_dicts(input)
         _, provider = self.client.chat.completions.model_select(messages=messages)
         return provider
+
+    def invoke(self, input: List[LanguageModelInput]) -> str:
+        return self._model_select(input)
+
+    def chain(self, input: List[LanguageModelInput]) -> str:
+        return self._model_select(input)
+
+    def batch(self, input: List[LanguageModelInput]) -> str:
+        """ [a9] do i need to change this to list[list[language_model_input]]? """
+        return self._model_select(input)
 
 def _convert_input_to_message_dicts(input: LanguageModelInput) -> List[Dict[str, str]]:
     if isinstance(input, PromptValue):
